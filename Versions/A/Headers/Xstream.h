86a258d6393f56d4fd930f050a1e6d685393f190
Torsten Zetzsche <torsten.zetzsche@siemens.com>
2015-02-04 15:00:51 +0100

    new: first version of Xstream framework

---
+++ Versions/A/Headers/Xstream.h
@@ -0,0 +1,13 @@
+//
+//  Xstream.h
+//  Xstream
+//
+//  Created by Torsten Zetzsche on 04.02.15.
+//  Copyright (c) 2015 com.thoughtworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Xstream : NSObject
+
+@end
